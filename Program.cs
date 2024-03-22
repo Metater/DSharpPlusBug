35b9ab05ee9f1019b138bec1159c78da15933643
Connor <61102529+Metater@users.noreply.github.com>
2024-03-22 18:47:26 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Logging;
+
+CancellationTokenSource cts = new();
+Console.CancelKeyPress += (s, e) =>
+{
+    cts.Cancel();
+    e.Cancel = true;
+};
+
+Console.WriteLine("Hello, World!");
+
+var token = File.ReadAllText("token.txt");
+
+var discord = new DiscordClient(new DiscordConfiguration()
+{
+    Token = token,
+    TokenType = TokenType.Bot,
+    Intents = DiscordIntents.All,
+    MinimumLogLevel = LogLevel.Information
+});
+
+// Init CommandsNext
+{
+    var commandsNext = discord.UseCommandsNext(new CommandsNextConfiguration()
+    {
+        StringPrefixes = ["$"]
+    });
+
+    commandsNext.RegisterCommands(Assembly.GetExecutingAssembly());
+}
+
+await discord.ConnectAsync();
+
+while (!cts.IsCancellationRequested)
+{
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+Console.WriteLine("Exiting!");
---
+++ TestModule.cs
@@ -0,0 +1,33 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace DSharpPlusBug;
+
+#pragma warning disable IDE0079 // Remove unnecessary suppression
+#pragma warning disable CA1822 // Mark members as static
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+[Group("test")]
+public class TestModule : BaseCommandModule
+{
+    private static int i = 0;
+    private static readonly object iLock = new();
+
+    [GroupCommand]
+    public async Task Test(CommandContext ctx)
+    {
+        int id;
+        lock (iLock)
+        {
+            id = i++;
+        }
+
+        await ctx.RespondAsync($"Start: {id}");
+        await Task.Delay(TimeSpan.FromSeconds(15));
+        await ctx.RespondAsync($"End: {id}");
+    }
+}
+
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+#pragma warning restore CA1822 // Mark members as static
+#pragma warning restore IDE0079 // Remove unnecessary suppression
